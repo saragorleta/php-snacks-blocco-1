af7ebdcd71b7c064dec18552364e312a0e49a5da
Sara <saragorleta@yahoo.com>
2021-02-25 16:57:02 +0100

    separati file

--- snack_2.php
+++ snack_2.php
@@ -1,32 +1,3 @@
-<!-- PHP Snack 1:
-Creiamo un array 'matches' contenente altri array i quali rappresentano delle partite di basket di un’ipotetica tappa del calendario.
-Ogni array della partita avrà una squadra di casa e una squadra ospite,
-punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
-Stampiamo a schermo tutte le partite con questo schema:
-Olimpia Milano - Cantù | 55 - 60 -->
-
-<?php
-$matches = [
-  [
-    'teamCasa'=> 'torino',
-    'teamOspite'=> 'milano',
-    'punteggio'=> '20 - 0'
-  ],
-  [
-    'teamCasa'=> 'napoli',
-    'teamOspite'=> 'roma',
-    'punteggio'=> '3 - 8'
-  ],
-  [
-    'teamCasa'=> 'cagliari',
-    'teamOspite'=> 'genova',
-    'punteggio'=> '15 - 21'
-  ],
-];
-
-$lunghezzaMatches = count($matches);
- ?>
- 
  <!-- PHP Snack 2:
  Passare come parametri GET name, mail e age e verificare
  (cercando i metodi che non conosciamo nella documentazione) che:
@@ -74,19 +45,7 @@ if (!is_numeric($validazioneAge)){
 </head>
 <body>
 
-  <?php for($i = 0; $i < $lunghezzaMatches; $i++) {
-    $teamCasa = $matches[$i]['teamCasa '];
-    $teamOspite = $matches[$i]['teamOspite '];
-    $punteggio=$matches[$i]['punteggio '];
-  ?>
-
-    <h2><?php echo $teamCasa ?></h2>
-     <span> - </span>
-    <h2><?php echo $teamOspite ?></h2>
-     <span> | </span>
-    <h3><?php echo $punteggio ?></h3>
-
-  <?php  }   ?>
+  
 
 
 </body>
