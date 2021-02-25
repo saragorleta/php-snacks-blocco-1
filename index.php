b9793c807be3aa25c0f3e5d66d5df6aff73bbc0e
Sara <saragorleta@yahoo.com>
2021-02-25 16:27:05 +0100

    init snack 2

--- index.php
+++ index.php
@@ -4,28 +4,64 @@ Ogni array della partita avrà una squadra di casa e una squadra ospite,
 punti fatti dalla squadra di casa e punti fatti dalla squadra ospite.
 Stampiamo a schermo tutte le partite con questo schema:
 Olimpia Milano - Cantù | 55 - 60 -->
+
 <?php
 $matches = [
   [
-    'teamCasa'=> 'torino'
-    'teamOspite'=> 'milano'
+    'teamCasa'=> 'torino',
+    'teamOspite'=> 'milano',
     'punteggio'=> '20 - 0'
   ],
   [
-    'teamCasa'=> 'napoli'
-    'teamOspite'=> 'roma'
+    'teamCasa'=> 'napoli',
+    'teamOspite'=> 'roma',
     'punteggio'=> '3 - 8'
   ],
   [
-    'teamCasa'=> 'cagliari'
-    'teamOspite'=> 'genova'
+    'teamCasa'=> 'cagliari',
+    'teamOspite'=> 'genova',
     'punteggio'=> '15 - 21'
   ],
 ];
 
 $lunghezzaMatches = count($matches);
  ?>
+ 
+ <!-- PHP Snack 2:
+ Passare come parametri GET name, mail e age e verificare
+ (cercando i metodi che non conosciamo nella documentazione) che:
+ 1. name sia più lungo di 3 caratteri,
+ 2. mail contenga un punto e una chiocciola
+ 3. age sia un numero.
+ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”. -->
 
+<?php
+
+$name = $_GET ['nome'];
+$mail = $_GET['mail'];
+
+
+$age = $_GET ['eta'];
+$validazioneAge= is_numeric($age);
+
+$messaggio = '';
+
+if(empty($name)) || (empty($mail)) || (empty($age)) {
+  $messaggio = "alcuni dati sono incompleti";
+}
+if (strleng($name < 3)){
+  $messaggio = "il nome deve essere più lungo di tre caratteri"
+}
+if ((strpos($mail, '@')) && (strpos($mail,'.'))) {
+  $messaggio = 'ok';
+} else {
+  $messaggio = "la mail non è ben formata";
+}
+if (!is_numeric($validazioneAge)){
+  $messaggio = "non stai inserendo un numero";
+}
+
+ ?>
 
 
 <!DOCTYPE html>
@@ -39,14 +75,15 @@ $lunghezzaMatches = count($matches);
 <body>
 
   <?php for($i = 0; $i < $lunghezzaMatches; $i++) {
-    $teamCasa = $matches[$i]['teamCasa'];
-    $teamOspite = $matches[$i]['teamOspite'];
-    $punteggio=$matches[$i]['punteggio'];
+    $teamCasa = $matches[$i]['teamCasa '];
+    $teamOspite = $matches[$i]['teamOspite '];
+    $punteggio=$matches[$i]['punteggio '];
   ?>
 
     <h2><?php echo $teamCasa ?></h2>
-    <?php echo <span> ?>
+     <span> - </span>
     <h2><?php echo $teamOspite ?></h2>
+     <span> | </span>
     <h3><?php echo $punteggio ?></h3>
 
   <?php  }   ?>
